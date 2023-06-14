0123d2b467b23b6738b115342821e60ce70461a5
Denis <nexan87@gmail.com>
2023-06-14 17:51:30 +0300

    Верстает слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,13 @@
 import { removeClassNoJs } from './remove-class-no-js.js';
 import { initButtonMenuActions } from './mobile-menu.js';
 import { undoDefaultActions } from './undo-default-actions.js';
+import { initSwiperActions } from './swiper.js';
+
 
 removeClassNoJs();
 undoDefaultActions();
 initButtonMenuActions();
+initSwiperActions();
+
+
 
